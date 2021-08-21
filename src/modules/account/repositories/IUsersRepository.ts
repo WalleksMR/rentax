9097e4895e2a93870860091c0491cbb9b16a089d
Walleks Miranda <walleksmiranda@gmail.com>
2021-08-21 15:58:39 -0300

    chore: create interface to repository users

---
+++ src/modules/account/repositories/IUsersRepository.ts
@@ -0,0 +1,19 @@
+interface IUsersRepositoryDTO {
+  name: string;
+  username: string;
+  password: string;
+  email: string;
+  driver_license: string;
+}
+
+interface IUsersRepository {
+  create({
+    name,
+    username,
+    password,
+    email,
+    driver_license,
+  }: IUsersRepositoryDTO): Promise<void>;
+}
+
+export { IUsersRepository, IUsersRepositoryDTO };
