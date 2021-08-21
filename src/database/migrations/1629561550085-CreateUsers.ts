de8b64e09fc371742d380ea63170789b213fffe1
Walleks Miranda <walleksmiranda@gmail.com>
2021-08-21 11:27:13 -0300

    refactor: was added tsyring in the listSpecifications

---
+++ src/database/migrations/1629561550085-CreateUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUsers1629561550085 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
