b8dd363fcfc657f401846efd60be0ec5528581b7
Walleks Miranda <walleksmiranda@gmail.com>
2021-08-21 22:59:27 -0300

    chore: was added bcrypt and created method list all users

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import { CreateUserController } from '../modules/account/useCases/createUser/CreateUserController';
+import { ListUserController } from '../modules/account/useCases/listUser/ListUserController';
 
 const usersRouter = Router();
 
 const createUserController = new CreateUserController();
+const listUserController = new ListUserController();
 
 usersRouter.post('/', createUserController.handle);
+usersRouter.get('/', listUserController.handle);
 
 export { usersRouter };
