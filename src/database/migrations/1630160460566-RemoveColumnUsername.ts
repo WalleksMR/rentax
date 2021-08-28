322de63c22440201752c73ca6490667867da6c1b
WalleksMR <walleksmiranda@gmail.com>
2021-08-28 11:32:32 -0300

    refactor: was removed column username in the Table Users

---
+++ src/database/migrations/1630160460566-RemoveColumnUsername.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export class RemoveColumnUsername1630160460566 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('users', 'username');
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'users',
+      new TableColumn({ name: 'username', type: 'varchar' })
+    );
+  }
+}
