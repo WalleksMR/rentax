6b3b37f5ba040fcaa0f0eb74d76c3a98720fda0e
Walleks Miranda <walleksmiranda@gmail.com>
2021-09-06 05:49:27 -0300

    chore: was added a new router to updated avatar of user

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,14 +1,27 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import upload from '../config/upload';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import { CreateUserController } from '../modules/account/useCases/createUser/CreateUserController';
 import { ListUserController } from '../modules/account/useCases/listUser/ListUserController';
+import { UpdateUserAvatarController } from '../modules/account/useCases/updateUserAvatar/UpdateUserAvatarController';
 
 const usersRouter = Router();
 
 const createUserController = new CreateUserController();
 const listUserController = new ListUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
+
+const uploadAvatar = multer(upload.upload('./tmp/avatar'));
 
 usersRouter.post('/', createUserController.handle);
 usersRouter.get('/', listUserController.handle);
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  uploadAvatar.single('avatar'),
+  updateUserAvatarController.handle
+);
 
 export { usersRouter };
