bf55d177478708fde4e5ad67a8e1c495cf31e6b9
Walleks Miranda <walleksmiranda@gmail.com>
2021-09-12 12:41:43 -0300

    refactor: adjusted import in the folder shared

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,11 +1,11 @@
 import { container } from 'tsyringe';
 
-import { UsersRepository } from '../../modules/account/repositories/implementations/UsersRepository';
-import { IUsersRepository } from '../../modules/account/repositories/IUsersRepository';
-import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
-import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository';
-import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
-import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
+import { UsersRepository } from '@modules/account/repositories/implementations/UsersRepository';
+import { IUsersRepository } from '@modules/account/repositories/IUsersRepository';
+import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
+import { CategoriesRepository } from '@modules/cars/repositories/implementations/CategoriesRepository';
+import { SpecificationsRepository } from '@modules/cars/repositories/implementations/SpecificationsRepository';
+import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository';
 
 // IUsersRepository
 container.registerSingleton<IUsersRepository>(
