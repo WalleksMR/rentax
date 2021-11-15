4961e450ed0de4ae478839fa3d21b36e3f1b0eef
Walleks Miranda <walleksmiranda@gmail.com>
2021-11-15 06:19:24 -0300

    choce: add CreateCarUseCase

---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -0,0 +1,46 @@
+import { inject, injectable } from 'tsyringe';
+
+import { ICreateCarDTO } from '@modules/cars/dto/ICreateCarDTO';
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { AppError } from '@shared/errors/AppError';
+
+@injectable()
+class CreateCarUseCase {
+  constructor(
+    @inject('CarsRepository')
+    private carsRepository: ICarsRepository
+  ) {}
+
+  async execute({
+    name,
+    description,
+    brand,
+    daily_rate,
+    fine_amount,
+    license_plate,
+    category_id,
+  }: ICreateCarDTO): Promise<Car> {
+    const carAlreadyExists = await this.carsRepository.findByLicensePlate(
+      license_plate
+    );
+
+    if (carAlreadyExists) {
+      throw new AppError('Car already exists');
+    }
+
+    const car = await this.carsRepository.create({
+      name,
+      description,
+      brand,
+      daily_rate,
+      fine_amount,
+      license_plate,
+      category_id,
+    });
+
+    return car;
+  }
+}
+
+export { CreateCarUseCase };
