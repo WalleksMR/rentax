246f90edb39a75831e8baa91db1d3e641ef4c1ad
Walleks Miranda <walleksmiranda@gmail.com>
2021-11-15 07:16:20 -0300

    choce: was created a seed to create a user admin

---
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -0,0 +1,23 @@
+import { hash } from 'bcrypt';
+import { getConnection } from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+
+import createConnection from '../index';
+
+async function create() {
+  const connection = await createConnection();
+
+  const id = uuidV4();
+  const password = await hash('admin', 8);
+
+  await connection.query(`
+    INSERT INTO USERS(id, name, email, password, driver_license, "isAdmin", created_at)
+    VALUES('${id}', 'admin', 'admin@admin.com', '${password}', 'XXXX-XXXX-XXXX' ,true, 'now()')
+  `);
+
+  await connection.close();
+}
+
+create().then(() => {
+  console.log('User admin was created with success!');
+});
