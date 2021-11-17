a51c3720abfdd45c269c86f196496620992b426d
Walleks Miranda <walleksmiranda@gmail.com>
2021-11-16 22:52:54 -0300

    choce: add middleware login and isAdmin in create car

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -2,10 +2,18 @@ import { Router } from 'express';
 
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
 
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+import { ensureIsAdmin } from '../middlewares/ensureIsAdmin';
+
 const createCarController = new CreateCarController();
 
 const carRouter = Router();
 
-carRouter.post('/', createCarController.handle);
+carRouter.post(
+  '/',
+  ensureAuthenticated,
+  ensureIsAdmin,
+  createCarController.handle
+);
 
 export { carRouter };
