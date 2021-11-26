46efa4d46ea69f4010ef5c1651c73b345d2df0d8
Walleks Miranda <walleksmiranda@gmail.com>
2021-11-26 00:08:59 -0300

    choce: add router GET to list car with available

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
+import { ListAvailableCarController } from '@modules/cars/useCases/listAvailableCar/ListAvailableCarController';
 
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import { ensureIsAdmin } from '../middlewares/ensureIsAdmin';
 
 const createCarController = new CreateCarController();
-
+const listAvailableCarController = new ListAvailableCarController();
 const carRouter = Router();
 
 carRouter.post(
@@ -15,5 +16,7 @@ carRouter.post(
   ensureIsAdmin,
   createCarController.handle
 );
+
+carRouter.get('/', listAvailableCarController.handle);
 
 export { carRouter };
