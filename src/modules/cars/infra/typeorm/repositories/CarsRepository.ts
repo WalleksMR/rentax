234f8c90213f593819689151ea44ee820e4f0917
Walleks Miranda <walleksmiranda@gmail.com>
2021-11-26 00:07:41 -0300

    choce: add implemetation findByAvailable in repo TypeORM

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -1,6 +1,7 @@
 import { Repository, getRepository } from 'typeorm';
 
 import { ICreateCarDTO } from '@modules/cars/dto/ICreateCarDTO';
+import { IListCarAvailable } from '@modules/cars/dto/IListCarAvailable';
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 
 import { Car } from '../entities/Car';
@@ -39,6 +40,23 @@ class CarsRepository implements ICarsRepository {
   async findByLicensePlate(license_plate: string): Promise<Car> {
     const car = await this.repository.findOne({ license_plate });
     return car;
+  }
+
+  async findByAvailable({
+    name,
+    brand,
+    category_id,
+  }: IListCarAvailable): Promise<Car[]> {
+    const carBuilder = this.repository
+      .createQueryBuilder('c')
+      .where('available = :available', { available: true });
+
+    if (name) carBuilder.andWhere('c.name = :name', { name });
+    if (brand) carBuilder.andWhere('c.brand = :brand', { brand });
+    if (category_id)
+      carBuilder.andWhere('c.category_id = :category_id', { category_id });
+    const cars = await carBuilder.getMany();
+    return cars;
   }
 }
 export { CarsRepository };
