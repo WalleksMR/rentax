e55e2f0e6a5b5c03abd4d09ab857ccef0dc7bd6f
Walleks Miranda <walleksmiranda@gmail.com>
2021-11-27 15:47:47 -0300

    choce: add ManyToMany Specifications in class Car

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -5,10 +5,13 @@ import {
   JoinColumn,
   ManyToOne,
   Entity,
+  ManyToMany,
+  JoinTable,
 } from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
 
 import { Category } from './Category';
+import { Specification } from './Specification';
 
 @Entity('cars')
 class Car {
@@ -42,6 +45,14 @@ class Car {
 
   @Column()
   category_id: string;
+
+  @ManyToMany(() => Specification)
+  @JoinTable({
+    name: 'specifications_cars',
+    joinColumns: [{ name: 'car_id' }], // Column of the table 'specifications_car' that belongs the this class 'cars'. [exemple: What's the column of the table specification_cars that belongs the column of the cars? Is car_id of the table specifications_id]
+    inverseJoinColumns: [{ name: 'specification_id' }], // Column of the 'specifications_cars' that belongs the class 'specifications',
+  })
+  specifications: Specification[];
 
   @CreateDateColumn()
   created_at: Date;
