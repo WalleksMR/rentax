4e78b207d8585034f7e4689324c34434affe792e
WalleksMR <walleksmiranda@gmail.com>
2022-01-19 18:38:08 -0300

    Add envirenment in typeorm infra

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,6 +1,9 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-unused-expressions */
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async (host = 'localhost'): Promise<Connection> => {
+export default async (host = 'database'): Promise<Connection> => {
+  process.env.NODE_ENV === 'development' ? (host = 'localhost') : host;
   const defaultOptions = await getConnectionOptions();
   return createConnection(
     Object.assign(defaultOptions, {
