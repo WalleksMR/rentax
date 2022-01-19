e111bd9f766323445687357f42d0348e963be27c
WalleksMR <walleksmiranda@gmail.com>
2022-01-19 18:51:16 -0300

    add router to add a new array of the image cars

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,21 +1,34 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import upload from '@config/upload';
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
+import { CreateSpecificationCarController } from '@modules/cars/useCases/createSpecificationCar/CreateSpecificationCarController';
 import { ListAvailableCarController } from '@modules/cars/useCases/listAvailableCar/ListAvailableCarController';
+import { UploadCarImagesController } from '@modules/cars/useCases/uploadCarImages/UploadCarImagesController';
 
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
-import { ensureIsAdmin } from '../middlewares/ensureIsAdmin';
+// import { ensureIsAdmin } from '../middlewares/ensureIsAdmin';
 
 const createCarController = new CreateCarController();
 const listAvailableCarController = new ListAvailableCarController();
+const createSpecificationCarController = new CreateSpecificationCarController();
+const uploadCarImagesController = new UploadCarImagesController();
 const carRouter = Router();
+const uploadCars = multer(upload.upload('./tmp/cars'));
 
 carRouter.post(
   '/',
   ensureAuthenticated,
-  ensureIsAdmin,
+  // ensureIsAdmin,
   createCarController.handle
 );
+carRouter.post(
+  '/images/:id',
+  uploadCars.array('images'),
+  uploadCarImagesController.handle
+);
+carRouter.post('/specifications/:id', createSpecificationCarController.handle);
 
 carRouter.get('/', listAvailableCarController.handle);
 
