b0a3d368fad4f1405c716d4a146abd848efd2c35
WalleksMR <walleksmiranda@gmail.com>
2022-01-19 18:46:51 -0300

    add register CarsImageRepository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,9 +2,11 @@ import { container } from 'tsyringe';
 
 import { UsersRepository } from '@modules/account/infra/typeorm/repositories/UsersRepository';
 import { IUsersRepository } from '@modules/account/repositories/IUsersRepository';
+import { CarsImageRepository } from '@modules/cars/infra/typeorm/repositories/CarsImageRepository';
 import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
 import { SpecificationsRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository';
+import { ICarsImageRepository } from '@modules/cars/repositories/ICarsImageRepository';
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
 import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository';
@@ -29,3 +31,9 @@ container.registerSingleton<ICategoriesRepository>(
 
 // ICarsRepository
 container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository);
+
+// CarImageRepository
+container.registerSingleton<ICarsImageRepository>(
+  'CarsImageRepository',
+  CarsImageRepository
+);
