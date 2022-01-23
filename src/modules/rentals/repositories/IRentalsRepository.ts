32f9d39e696590d0fa5059dc2f4dac8058612307
WalleksMR <walleksmiranda@gmail.com>
2022-01-22 23:53:29 -0300

    create interface rental repository

---
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -0,0 +1,12 @@
+import { ICreateRentalDTO } from '../dto/ICreateRentalDTO';
+import { Rental } from '../infra/typeorm/entities/Rental';
+
+export interface IRentalsRepository {
+  create({
+    car_id,
+    user_id,
+    expected_return_date,
+  }: ICreateRentalDTO): Promise<Rental>;
+  findOpenRentalByCar(car_id: string): Promise<Rental>;
+  findOpenRentalByUser(user_id: string): Promise<Rental>;
+}
