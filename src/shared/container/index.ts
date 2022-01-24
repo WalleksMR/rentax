7caad94efa6a5154b326beab33caf6fd431e71c8
WalleksMR <walleksmiranda@gmail.com>
2022-01-24 18:05:41 -0300

    add a new register singleton Rental

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -10,6 +10,15 @@ import { ICarsImageRepository } from '@modules/cars/repositories/ICarsImageRepos
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
 import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository';
+import { RentalsRepository } from '@modules/rentals/infra/typeorm/repositories/RentalsRepository';
+import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository';
+import '@shared/container/providers';
+
+// RentalsRepository
+container.registerSingleton<IRentalsRepository>(
+  'RentalsRepository',
+  RentalsRepository
+);
 
 // IUsersRepository
 container.registerSingleton<IUsersRepository>(
