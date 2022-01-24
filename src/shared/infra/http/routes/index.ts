e4ac8107e66f835462d99ba58fd2e4d0b19b817e
WalleksMR <walleksmiranda@gmail.com>
2022-01-24 18:08:29 -0300

    add a new router to rental

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,16 +2,18 @@ import { Router } from 'express';
 
 import { carRouter } from './cars.routes';
 import { categoriesRouter } from './categories.routes';
+import { rentalRoutes } from './rental.routes';
 import { sessionRouter } from './session.routes';
 import { specificationRouter } from './specification.routes';
 import { usersRouter } from './users.routes';
 
 const routes = Router();
 
+routes.use('/sessions', sessionRouter);
+routes.use('/users', usersRouter);
 routes.use('/categories', categoriesRouter);
 routes.use('/specifications', specificationRouter);
-routes.use('/users', usersRouter);
 routes.use('/cars', carRouter);
-routes.use('/sessions', sessionRouter);
+routes.use('/rentals', rentalRoutes);
 
 export { routes };
