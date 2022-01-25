29427f8a7a590bf3c5088517aab9964ed16925b8
WalleksMR <walleksmiranda@gmail.com>
2022-01-25 20:50:06 -0300

    add authenticated and admin in route category post

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -4,13 +4,20 @@ import multer from 'multer';
 import createCategoryController from '@modules/cars/useCases/createCategory';
 import { ImportCategoryController } from '@modules/cars/useCases/importCategory/ImportCategoryController';
 import listCategoriesController from '@modules/cars/useCases/listCategories';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+import { ensureIsAdmin } from '../middlewares/ensureIsAdmin';
 
 const categoriesRouter = Router();
 const upload = multer({ dest: './tmp' });
 const importCategoryController = new ImportCategoryController();
-categoriesRouter.post('/', (request, response) => {
-  return createCategoryController().handle(request, response);
-});
+categoriesRouter.post(
+  '/',
+  ensureAuthenticated,
+  ensureIsAdmin,
+  (request, response) => {
+    return createCategoryController().handle(request, response);
+  }
+);
 
 categoriesRouter.get('/', (request, response) => {
   return listCategoriesController().handle(request, response);
