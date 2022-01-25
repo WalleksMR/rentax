09eb51e3065988302c486aa4cc09611d2391e97d
WalleksMR <walleksmiranda@gmail.com>
2022-01-25 20:54:28 -0300

    app and server layer isolated

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,35 +1,6 @@
-import 'dotenv/config';
-import 'reflect-metadata';
-import express, { NextFunction, Request, Response } from 'express';
-import 'express-async-errors';
-
-import '@shared/container';
-
-import { AppError } from '@shared/errors/AppError';
-import createConnection from '@shared/infra/typeorm';
-
-import { routes } from './routes';
-
-createConnection();
+import { app } from './app';
 
 const port = 3333;
-const app = express();
-app.use(express.json());
-app.use(routes);
-
-app.use(
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({ message: err.message });
-    }
-
-    return response.status(500).json({
-      status: 'error',
-      message: `Internal server error - ${err.message}`,
-    });
-  }
-);
 
 app.listen(port, () => {
   console.log(`Server is running on port 3333`);
