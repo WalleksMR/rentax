8414f8dd5fd64c5f75efe8cd91bc3a48450394ed
WalleksMR <walleksmiranda@gmail.com>
2022-01-26 14:26:25 -0300

    refactor: disable synchronize typeorm

--- ormconfig.ts
+++ ormconfig.ts
@@ -5,7 +5,7 @@ export default {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  synchronize: true,
+  synchronize: false,
   migrations: ['./src/shared/**/migrations/*.ts'],
   entities: ['src/modules/**/entities/*.ts'],
   cli: {
