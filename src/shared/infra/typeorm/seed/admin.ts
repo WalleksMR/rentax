2bd7abe5cb0822cbc765492095708f5c1eab0b3c
WalleksMR <walleksmiranda@gmail.com>
2022-01-30 22:09:39 -0300

    refactor: adjusted seed admin

--- src/shared/infra/typeorm/seed/admin.ts
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -4,7 +4,7 @@ import { v4 as uuidV4 } from 'uuid';
 import createConnection from '../index';
 
 async function create() {
-  const connection = await createConnection();
+  const connection = await createConnection('localhost');
 
   const id = uuidV4();
   const password = await hash('admin', 8);
@@ -17,6 +17,9 @@ async function create() {
   await connection.close();
 }
 
-create().then(() => {
+try {
+  create();
   console.log('User admin was created with success!');
-});
+} catch (err) {
+  console.log(err);
+}
