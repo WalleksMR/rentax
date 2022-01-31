57a975dabcae558d71de0168bcb3b53c5301db9e
WalleksMR <walleksmiranda@gmail.com>
2022-01-30 22:14:12 -0300

    choce: add a new functionality findById Rental in Repository

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -11,28 +11,42 @@ class RentalsRepository implements IRentalsRepository {
   constructor() {
     this.repository = getRepository(Rental);
   }
+
   async create({
     car_id,
     user_id,
     expected_return_date,
+    end_date,
+    id,
+    total,
   }: ICreateRentalDTO): Promise<Rental> {
     const rental = this.repository.create({
       car_id,
       user_id,
       expected_return_date,
+      end_date,
+      id,
+      total,
     });
-    console.log(rental);
 
     await this.repository.save(rental);
     return rental;
   }
   async findOpenRentalByCar(car_id: string): Promise<Rental> {
-    const rentalOpenCar = await this.repository.findOne({ car_id });
+    const rentalOpenCar = await this.repository.findOne({
+      where: { car_id, end_date: null },
+    });
     return rentalOpenCar;
   }
   async findOpenRentalByUser(user_id: string): Promise<Rental> {
-    const rentalOpenUser = await this.repository.findOne({ user_id });
+    const rentalOpenUser = await this.repository.findOne({
+      where: { user_id, end_date: null },
+    });
     return rentalOpenUser;
+  }
+
+  findById(id: string): Promise<Rental> {
+    return this.repository.findOne({ id });
   }
 }
 
