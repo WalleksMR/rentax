561d26de10d0d34996fa2da065779fa09b7bad7f
WalleksMR <walleksmiranda@gmail.com>
2022-02-01 21:48:42 -0300

    test: create rental

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -5,41 +5,72 @@ import { DayjsDateProvider } from '@shared/container/providers/DateProvider/impl
 import { AppError } from '@shared/errors/AppError';
 
 import { CreateRentalUseCase } from './CreateRentalUseCase';
+import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
+import { CategoriesRepositoryInMemory } from '@modules/cars/repositories/in-memory/CategoriesRepositoryInMemory';
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
+import { Category } from '@modules/cars/infra/typeorm/entities/Category';
 
 describe('Create Rental', () => {
   let createRentalUseCase: CreateRentalUseCase;
+  let carsRepositoryInMemory: CarsRepositoryInMemory;
   let dayJsDateProvider: DayjsDateProvider;
   let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
+  let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
   const Hours24After = dayjs().add(1, 'day').toDate();
-  beforeEach(() => {
+
+  let car: Car;
+  let category: Category;
+
+  beforeEach(async () => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
     rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
     dayJsDateProvider = new DayjsDateProvider();
     createRentalUseCase = new CreateRentalUseCase(
       rentalsRepositoryInMemory,
-      dayJsDateProvider
+      dayJsDateProvider,
+      carsRepositoryInMemory
     );
+
+    category = await categoriesRepositoryInMemory.create({
+      name: 'SUV',
+      description: 'Top SUV',
+    });
+
+    car = await carsRepositoryInMemory.create({
+      name: 'Car 01',
+      description: 'Car 01 description',
+      brand: 'Brand Car 01',
+      daily_rate: 122,
+      fine_amount: 11111,
+      license_plate: 'ABB-CCCD',
+      category_id: category.id,
+    });
   });
+
   it('should be able to create a new rental', async () => {
     const rental = await createRentalUseCase.execute({
       user_id: '999',
-      car_id: '344',
+      car_id: car.id,
       expected_return_date: Hours24After,
     });
 
     expect(rental).toHaveProperty('id');
     expect(rental).toHaveProperty('start_date');
+    expect(car.available).toBe(false);
   });
 
   it('should not be able to create a new rental if there is another open to the same user', () => {
     expect(async () => {
       await createRentalUseCase.execute({
         user_id: '999',
-        car_id: '344',
+        car_id: car.id,
         expected_return_date: Hours24After,
       });
       await createRentalUseCase.execute({
         user_id: '999',
-        car_id: '344',
+        car_id: car.id,
         expected_return_date: Hours24After,
       });
     }).rejects.toBeInstanceOf(AppError);
@@ -49,12 +80,12 @@ describe('Create Rental', () => {
     expect(async () => {
       await createRentalUseCase.execute({
         user_id: '222',
-        car_id: '344',
+        car_id: car.id,
         expected_return_date: Hours24After,
       });
       await createRentalUseCase.execute({
         user_id: '999',
-        car_id: '344',
+        car_id: car.id,
         expected_return_date: Hours24After,
       });
     }).rejects.toBeInstanceOf(AppError);
@@ -64,7 +95,7 @@ describe('Create Rental', () => {
     expect(async () => {
       await createRentalUseCase.execute({
         user_id: '222',
-        car_id: '344',
+        car_id: car.id,
         expected_return_date: dayjs().toDate(),
       });
     }).rejects.toBeInstanceOf(AppError);
