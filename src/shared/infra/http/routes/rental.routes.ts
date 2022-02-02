2d55e30cb79db2f012f2dd1ef0867e52828cfc68
WalleksMR <walleksmiranda@gmail.com>
2022-02-02 15:56:48 -0300

    choce: add new router to post devolution and get list rental by user

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -3,9 +3,20 @@ import { Router } from 'express';
 import { CreateRentalController } from '@modules/rentals/useCases/createRental/CreateRentalCrontoller';
 
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+import { ListRentalUseController } from '@modules/rentals/useCases/listRentalUser/ListRentalUserController';
+import { DevolutionRentalController } from '@modules/rentals/useCases/devolutionRental/DevolutionRentalController';
 
 const rentalRoutes = Router();
 const createRentalController = new CreateRentalController();
+const listRentalUseController = new ListRentalUseController();
+const devolutionRentalController = new DevolutionRentalController();
+
 rentalRoutes.post('/', ensureAuthenticated, createRentalController.handle);
+rentalRoutes.get('/user', ensureAuthenticated, listRentalUseController.handle);
+rentalRoutes.post(
+  '/devolution/:id',
+  ensureAuthenticated,
+  devolutionRentalController.handle
+);
 
 export { rentalRoutes };
