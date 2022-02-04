116460f0dcd92fb94d7367bbf9c4599669996ddb
WalleksMR <walleksmiranda@gmail.com>
2022-02-04 10:17:39 -0300

    choce: add two method

--- src/modules/account/repositories/IUsersTokenRepository.ts
+++ src/modules/account/repositories/IUsersTokenRepository.ts
@@ -7,4 +7,10 @@ export interface IUsersTokenRepository {
     refresh_token,
     user_id,
   }: ICreateUserTokenDTO): Promise<UserToken>;
+  findByUserIdAndRefreshToken(
+    user_id: string,
+    refresh_token: string
+  ): Promise<UserToken>;
+
+  deleteById(id: string): Promise<void>;
 }
