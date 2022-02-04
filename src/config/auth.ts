87a9ed6d57d64ea7882f8d2a0c0406f8cbd0b456
WalleksMR <walleksmiranda@gmail.com>
2022-02-04 10:14:35 -0300

    refactor: isolated layer, data of auth

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  expires_in_token: '15m',
+  secret_refresh_token: 'igniteRefreshToken',
+  secret_token: 'ignite',
+  expires_in_refresh_token: '30d',
+  expires_refresh_token_days: 30,
+};
