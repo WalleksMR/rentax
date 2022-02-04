023849239c8d8cfa28f6797eaa4cabecf26ab045
WalleksMR <walleksmiranda@gmail.com>
2022-02-04 10:23:04 -0300

    choce: was added a new router to created a new refresh token

--- src/shared/infra/http/routes/session.routes.ts
+++ src/shared/infra/http/routes/session.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
 import { CreateAuthenticateController } from '@modules/account/useCases/createAuthenticate/CreateAuthenticateController';
+import { RefreshTokenController } from '@modules/account/useCases/refreshToken/RefreshTokenController';
 
 const sessionRouter = Router();
 const createAuthenticateController = new CreateAuthenticateController();
+const refreshTokenController = new RefreshTokenController();
+
 sessionRouter.post('/', createAuthenticateController.handle);
+sessionRouter.post('/refresh-token', refreshTokenController.handle);
 
 export { sessionRouter };
