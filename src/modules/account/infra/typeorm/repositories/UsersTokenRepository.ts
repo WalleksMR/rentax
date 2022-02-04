2a5624d418848fd3edb2152bf85328483dab9523
WalleksMR <walleksmiranda@gmail.com>
2022-02-04 10:17:04 -0300

    choce: add a new repository to TokenRefresh

---
+++ src/modules/account/infra/typeorm/repositories/UsersTokenRepository.ts
@@ -0,0 +1,39 @@
+import { ICreateUserTokenDTO } from '@modules/account/dto/ICreateUserTokenDTO';
+import { IUsersTokenRepository } from '@modules/account/repositories/IUsersTokenRepository';
+import { getRepository, Repository } from 'typeorm';
+import { UserToken } from '../entities/UserToken';
+
+export class UsersTokenRepository implements IUsersTokenRepository {
+  private repository: Repository<UserToken>;
+
+  constructor() {
+    this.repository = getRepository(UserToken);
+  }
+
+  async create({
+    expires_date,
+    refresh_token,
+    user_id,
+  }: ICreateUserTokenDTO): Promise<UserToken> {
+    const userToken = this.repository.create({
+      expires_date,
+      refresh_token,
+      user_id,
+    });
+
+    await this.repository.save(userToken);
+
+    return userToken;
+  }
+
+  async findByUserIdAndRefreshToken(
+    user_id: string,
+    refresh_token: string
+  ): Promise<UserToken> {
+    return this.repository.findOne({ user_id, refresh_token });
+  }
+
+  async deleteById(id: string): Promise<void> {
+    await this.repository.delete(id);
+  }
+}
