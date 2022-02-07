c355694625eebb5bbfa2fe6522fdd4cb84d490ac
WalleksMR <walleksmiranda@gmail.com>
2022-02-07 19:38:59 -0300

    choce: was added new router to reset password

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,7 +1,10 @@
+import { ResetPasswordUserController } from '@modules/account/useCases/resetPasswordUser/ResetPasswordUserController';
 import { SendForgotPasswordMailController } from '@modules/account/useCases/sendForgotPasswordMail/SendForgotPasswordMailController';
 import { Router } from 'express';
 
 export const passwordRouter = Router();
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
+const resetPasswordUserController = new ResetPasswordUserController();
 
 passwordRouter.post('/forgot', sendForgotPasswordMailController.handle);
+passwordRouter.post('/reset', resetPasswordUserController.handle);
