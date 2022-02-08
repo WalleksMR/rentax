caff7093f3eb07158e2e557635c8c558a37c684f
WalleksMR <walleksmiranda@gmail.com>
2022-02-08 15:26:00 -0300

    refactor: add a new token to authenticated

--- src/modules/account/useCases/refreshToken/RefreshTokenUseCase.ts
+++ src/modules/account/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -5,9 +5,10 @@ import { AppError } from '@shared/errors/AppError';
 import { sign, verify } from 'jsonwebtoken';
 import { inject, injectable } from 'tsyringe';
 
-// interface ITokenResponse {
-//   refresh_token: string;
-// }
+interface ITokenResponse {
+  refresh_token: string;
+  newToken: string;
+}
 
 interface IPayload {
   sub: string;
@@ -23,7 +24,7 @@ export class RefreshTokenUseCase {
     private dateProvider: IDateProvider
   ) {}
 
-  async execute(token: string): Promise<string> {
+  async execute(token: string): Promise<ITokenResponse> {
     const { email, sub } = verify(token, auth.secret_refresh_token) as IPayload;
 
     const user_id = sub;
@@ -45,6 +46,11 @@ export class RefreshTokenUseCase {
       expiresIn: auth.expires_in_refresh_token,
     });
 
+    const newToken = sign({}, auth.secret_token, {
+      subject: user_id,
+      expiresIn: auth.expires_in_token,
+    });
+
     const expires_date = this.dateProvider.addDays(
       auth.expires_refresh_token_days
     );
@@ -55,6 +61,6 @@ export class RefreshTokenUseCase {
       expires_date,
     });
 
-    return refresh_token;
+    return { refresh_token, newToken };
   }
 }
