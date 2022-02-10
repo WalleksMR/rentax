184668e1ae833f13ca72815cff3a587f906121ff
WalleksMR <walleksmiranda@gmail.com>
2022-02-10 13:33:53 -0300

    refactor: To pass in tests the send email

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -6,25 +6,31 @@ import { IMailProvider, ISendMail } from '../IMailProvider';
 export class EtherealMailProvider implements IMailProvider {
   private client: Transporter;
   constructor() {
-    nodemailer
-      .createTestAccount()
-      .then((account) => {
-        const transporter = nodemailer.createTransport({
-          host: account.smtp.host,
-          port: account.smtp.port,
-          secure: account.smtp.secure,
-          auth: {
-            user: account.user,
-            pass: account.pass,
-          },
-        });
-        this.client = transporter;
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    this.createClient();
   }
+
+  private async createClient() {
+    try {
+      const account = await nodemailer.createTestAccount();
+
+      this.client = nodemailer.createTransport({
+        host: account.smtp.host,
+        port: account.smtp.port,
+        secure: account.smtp.secure,
+        auth: {
+          user: account.user,
+          pass: account.pass,
+        },
+      });
+    } catch (error) {
+      console.log(`EtherealMailProvider - Error: \n${error}`);
+    }
+  }
+
   async sendMail({ to, subject, variables, path }: ISendMail): Promise<void> {
+    if (!this.client) {
+      await this.createClient();
+    }
     const templateFileContent = fs.readFileSync(path).toString('utf-8');
     const templateParse = handlebars.compile(templateFileContent);
 
