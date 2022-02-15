24422bdbaa635c4f56bf7e3597067b6c778cc451
WalleksMR <walleksmiranda@gmail.com>
2022-02-15 16:26:15 -0300

    refactor: adjust fileContent and bucket

--- src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
+++ src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
@@ -9,16 +9,16 @@ import { IStorageProvider } from '../IStorageProvider';
 export class S3StorageProvider implements IStorageProvider {
   private client: S3;
   constructor() {
-    this.client = new S3({ region: process.env.AWS_REGION });
+    this.client = new S3({ region: process.env.AWS_BUCKET_REGION });
   }
   async save(file: string, folder: string): Promise<string> {
     const originalName = resolve(upload.tmpFolder, file);
-    const fileContent = fs.promises.readFile(originalName);
+    const fileContent = await fs.promises.readFile(originalName);
     const ContentType = mime.getType(originalName);
 
     await this.client
       .putObject({
-        Bucket: `${process.env.AWS_BUCKET}`,
+        Bucket: `${process.env.AWS_BUCKET}/${folder}`,
         Key: file,
         ACL: 'public-read',
         Body: fileContent,
@@ -29,6 +29,7 @@ export class S3StorageProvider implements IStorageProvider {
     await fs.promises.unlink(originalName);
     return file;
   }
+
   async delete(file: string, folder: string): Promise<void> {
     await this.client
       .deleteObject({
