a2d31954e452c2d81767d0d918497a75bb2811dc
WalleksMR <walleksmiranda@gmail.com>
2022-03-02 11:48:47 -0300

    refactor: add avatar_url

--- src/modules/account/mapper/UserMapper.ts
+++ src/modules/account/mapper/UserMapper.ts
@@ -1,7 +1,24 @@
-import { ICreateUserDTO } from '../dto/ICreateUserDTO';
+import { IUserResponseDTO } from '../dto/IUserResponseDTO';
+import { instanceToInstance } from 'class-transformer';
+import { User } from '../infra/typeorm/entities/User';
 
 export class UseMapper {
-  static toDTO({ id, name, email, driver_license, avatar }: ICreateUserDTO) {
-    return { id, name, email, driver_license, avatar };
+  static toDTO({
+    id,
+    name,
+    email,
+    driver_license,
+    avatar,
+    avatar_url,
+  }: User): IUserResponseDTO {
+    const user = instanceToInstance({
+      id,
+      name,
+      email,
+      driver_license,
+      avatar,
+      avatar_url,
+    });
+    return user;
   }
 }
