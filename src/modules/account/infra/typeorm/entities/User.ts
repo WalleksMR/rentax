4551ba5143f25a417755f7cc2c8752442c745fbc
WalleksMR <walleksmiranda@gmail.com>
2022-03-02 11:48:22 -0300

    chore: add function to get url profile of user

--- src/modules/account/infra/typeorm/entities/User.ts
+++ src/modules/account/infra/typeorm/entities/User.ts
@@ -1,3 +1,4 @@
+import { Expose } from 'class-transformer';
 import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
 
@@ -22,6 +23,18 @@ class User {
     default: false,
   })
   isAdmin: boolean;
+
+  @Expose({ name: 'avatar_url' })
+  avatar_url(): string {
+    switch (process.env.DISK_STORAGE) {
+      case 'local':
+        return `${process.env.APP_API_URL}/avatar/${this.avatar}`;
+      case 's3':
+        return `${process.env.AWS_BUCKET_URL}/avatar/${this.avatar}`;
+      default:
+        null;
+    }
+  }
 
   @Column({
     nullable: true,
