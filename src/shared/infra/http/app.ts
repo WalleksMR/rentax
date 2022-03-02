c6af0209d9fc5c3ccadf8364bcfa1c80d1ab8a0f
WalleksMR <walleksmiranda@gmail.com>
2022-03-02 11:08:17 -0300

    chore: add sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,6 +3,8 @@ import 'dotenv/config';
 import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 import swaggerUi from 'swagger-ui-express';
 import swaggerFile from '../../../swagger.json';
 import '@shared/container';
@@ -15,12 +17,27 @@ import { routes } from './routes';
 
 createConnection();
 const app = express();
+
 app.use(rateLimiter);
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(cors());
 
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(routes);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
